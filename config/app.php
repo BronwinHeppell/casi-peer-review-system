6f7437968f23e00881fa2d5954e302c85349f0f1
Bronwin Heppell <bronwinheppell98@gmail.com>
2025-02-11 13:09:31 +0200

    update config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://internship-bronwin.casi-app.com'),
 
     /*
     |--------------------------------------------------------------------------
